28a3032b650b1f326ebf8c2e4974df227a46b893
周启豪 <bighao1996@163.com>
2019-12-14 00:56:19 +0800

    稀疏数组以及练习

---
+++ DataStructures/src/main/java/com/bighao/sparsearray/SparseArray.java
@@ -0,0 +1,129 @@
+package com.bighao.sparsearray;
+
+/**
+ * @Author: bighao周启豪
+ * @Date: 2019/12/13 22:07
+ * @Version 1.0
+ *
+ * 稀疏数组SparseArray
+ * 图解在Execl文档中第336行起 video 7-9
+ * 因为该二维数组的很多值是默认值0, 因此记录了很多没有意义的数据.->稀疏数组。
+ *
+ * 1.基本介绍
+ *  当一个数组中大部分元素为０，或者为同一个值的数组时，可以使用稀疏数组来保存该数组。
+ *
+ * 2.稀疏数组的处理方法是:
+ * 记录数组一共有几行几列，有多少个不同的值
+ * 把具有不同值的元素的行列及值记录在一个小规模的数组中，从而缩小程序的规模
+ *
+ * 3.转换:
+ * 二维数组 转 稀疏数组的思路
+ *  1. 遍历  原始的二维数组，得到有效数据的个数 sum
+ *  2. 根据sum 就可以创建 稀疏数组 sparseArr   int[sum + 1] [3]
+ *  3. 将二维数组的有效数据数据存入到 稀疏数组
+ *          row   col   val
+ *      0   11     11    2 --第一行用来保存原始的二维数组总共有几行 几列 几个非0的值
+ *      1   1      2     1
+ *      2   2      3     2
+ *
+ * 稀疏数组转原始的二维数组的思路
+ *  1. 先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组，比如上面的  chessArr2 = int [11][11]
+ *  2. 在读取稀疏数组后几行的数据，并赋给 原始的二维数组 即可
+ *
+ *
+ * 练习要求：
+ * 在前面的基础上，将稀疏数组保存到磁盘上，比如 map.data
+ * 恢复原来的数组时，读取map.data 进行恢复
+ */
+public class SparseArray {
+    public static void main(String[] args) {
+        // 创建一个原始的二维数组 11*11
+        // 0-表示没有棋子 1-黑子 2-蓝子
+        int[][] chessArr1 = new int[11][11];
+        chessArr1[1][2] = 1;
+        chessArr1[2][3] = 2;
+        chessArr1[4][5] = 2;
+
+        // 输出原始的二维数组
+        System.out.println("原始的二维数组=======>");
+        for(int[] row : chessArr1) {
+            for(int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        /** 将二维数组 转 稀疏数组的思路 */
+        // 1.先遍历二维数组 得到非0数据的个数
+        int sum = 0;
+        for(int i = 0; i < chessArr1.length; i++) {
+            for(int j = 0; j < chessArr1[i].length; j++) {
+                if(chessArr1[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        System.out.println("sum=" + sum);
+
+        // 2.创建对应的稀疏数组
+        // 稀疏数组第一行需要来描述原来的二维数组，因此需要+1 3是稀疏数组固定用来描述原来数据的列数
+        int sparseArr[][] = new int[sum + 1][3];
+        // 给稀疏数组赋值
+        sparseArr[0][0] = 11;
+        sparseArr[0][1] = 11;
+        sparseArr[0][2] = sum;
+
+        // 遍历二维数组，将非0的值存放到sparseArr中
+        int count = 0; // count 用于记录是第几个非0数据
+        for (int i = 0; i < chessArr1.length; i++) {
+            for (int j = 0; j < chessArr1[i].length; j++) {
+                if (chessArr1[i][j] != 0) {
+                    count++; // 第一行用来存元数据
+                    sparseArr[count][0] = i;
+                    sparseArr[count][1] = j;
+                    sparseArr[count][2] = chessArr1[i][j];
+                }
+            }
+        }
+
+        // 输出稀疏数组的形式
+        System.out.println();
+        System.out.println("得到的稀疏数组为========>");
+        for (int i = 0; i < sparseArr.length; i++) {
+            System.out.printf("%d\t%d\t%d\t\n", sparseArr[i][0], sparseArr[i][1],  sparseArr[i][2]);
+        }
+        System.out.println();
+
+
+        /** 将稀疏数组 恢复成 原始的二维数组 */
+        /*
+          1. 先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组，比如上面的  chessArr2 = int [11][11]
+          2. 在读取稀疏数组后几行的数据，并赋给 原始的二维数组 即可.
+        */
+
+        // 1.先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组
+        int rowCount = sparseArr[0][0]; //原始数组的行数
+        int colCount = sparseArr[0][1]; //原始数组的列数
+        int chessArr2[][] = new int[rowCount][colCount];
+
+        // 2.在读取稀疏数组后几行(从第二行开始)的数据，并赋给 原始的二维数组 即可
+        for (int i = 1; i < sparseArr.length; i++) {
+            int rowNum = sparseArr[i][0]; //原来数据的行号
+            int colNum = sparseArr[i][1]; //原来数据的列号
+            chessArr2[rowNum][colNum] = sparseArr[i][2];
+        }
+
+        // 输出恢复后的二维数组
+        System.out.println();
+        System.out.println("恢复后的二维数组");
+        for(int[] row : chessArr2) {
+            for(int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+    }
+
+
+}
---
+++ DataStructures/src/main/java/com/bighao/sparsearray/SparseArrayOptimization.java
@@ -0,0 +1,223 @@
+package com.bighao.sparsearray;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: bighao周启豪
+ * @Date: 2019/12/13 22:07
+ * @Version 1.0
+ */
+
+/**
+ * 稀疏数组
+ * 加上练习
+ */
+public class SparseArrayOptimization {
+    public static void main(String[] args) {
+        // 创建一个原始的二维数组 11*11
+        // 0-表示没有棋子 1-黑子 2-蓝子
+        int[][] chessArr1 = new int[11][11];
+        chessArr1[1][2] = 1;
+        chessArr1[2][3] = 2;
+        chessArr1[4][5] = 2;
+        // 将原来的二维数组转为稀疏数组
+        int[][] sparseArr = getSparseArray(chessArr1);
+        // 将稀疏数组保存到磁盘
+        saveSparseArrToFile(sparseArr);
+
+        /** 将稀疏数组 恢复成 原始的二维数组 */
+        // 1.先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组
+        // 读取文件里的内容转为稀疏数组
+        int[][] sparseArr2 = readFileToSparseArr();
+        // 将稀疏数组恢复为原来的二维数组
+        int[][] chessArr2 = recoverArray(sparseArr2);
+
+        // 输出恢复后的二维数组
+        System.out.println();
+        System.out.println("恢复后的二维数组");
+        for(int[] row : chessArr2) {
+            for(int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static int[][] recoverArray(int[][] sparseArr2) {
+        int rowCount = sparseArr2[0][0]; //原始数组的行数
+        int colCount = sparseArr2[0][1]; //原始数组的列数
+        int chessArr2[][] = new int[rowCount][colCount];
+
+        // 2.在读取稀疏数组后几行(从第二行开始)的数据，并赋给 原始的二维数组 即可
+        for (int i = 1; i < sparseArr2.length; i++) {
+            int rowNum = sparseArr2[i][0]; //原来数据的行号
+            int colNum = sparseArr2[i][1]; //原来数据的列号
+            chessArr2[rowNum][colNum] = sparseArr2[i][2];
+        }
+        return chessArr2;
+    }
+
+    /** 将原来的二维数组转为稀疏数组 */
+    private static int[][] getSparseArray(int[][] chessArr1) {
+        // 输出原始的二维数组
+        System.out.println("原始的二维数组=======>");
+        for(int[] row : chessArr1) {
+            for(int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        /** 将二维数组 转 稀疏数组的思路 */
+        // 1.先遍历二维数组 得到非0数据的个数
+        int sum = 0;
+        for(int i = 0; i < chessArr1.length; i++) {
+            for(int j = 0; j < chessArr1[i].length; j++) {
+                if(chessArr1[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        System.out.println("sum=" + sum);
+
+        // 2.创建对应的稀疏数组
+        // 稀疏数组第一行需要来描述原来的二维数组，因此需要+1 3是稀疏数组固定用来描述原来数据的列数
+        int sparseArr[][] = new int[sum + 1][3];
+        // 给稀疏数组赋值
+        sparseArr[0][0] = 11;
+        sparseArr[0][1] = 11;
+        sparseArr[0][2] = sum;
+
+        // 遍历二维数组，将非0的值存放到sparseArr中
+        int count = 0; // count 用于记录是第几个非0数据
+        for (int i = 0; i < chessArr1.length; i++) {
+            for (int j = 0; j < chessArr1[i].length; j++) {
+                if (chessArr1[i][j] != 0) {
+                    count++; // 第一行用来存元数据
+                    sparseArr[count][0] = i;
+                    sparseArr[count][1] = j;
+                    sparseArr[count][2] = chessArr1[i][j];
+                }
+            }
+        }
+
+        // 输出稀疏数组的形式
+        System.out.println();
+        System.out.println("得到的稀疏数组为========>");
+        for (int i = 0; i < sparseArr.length; i++) {
+            System.out.printf("%d\t%d\t%d\t\n", sparseArr[i][0], sparseArr[i][1],  sparseArr[i][2]);
+        }
+        System.out.println();
+
+        return sparseArr;
+    }
+
+    /** 测试读取文件转为稀疏数组 */
+    @Test
+    public void test() {
+        int[][] sparseArr = readFileToSparseArr();
+        for(int[] row : sparseArr) {
+            for(int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+    }
+
+    /** 将文件中的数据转为稀疏数组 */
+    public static int[][] readFileToSparseArr() {
+        BufferedReader buf = null;
+        int[][] sparseArr = null;
+        try {
+            // 获取文件保存路径
+            String currentJavaFilePath = getCurrentJavaFilePath();
+            File file = new File("D:\\java workspace\\interview_learn_work\\韩顺平老师课程代码\\Algorithm_DataStructure\\map.txt");
+
+            buf = new BufferedReader(new FileReader(file));
+            String tempStr = null;
+            // 将每一行数据先添加到该集合里
+            List<String> list = new ArrayList<>();
+            while ((tempStr = buf.readLine()) != null) {
+                list.add(tempStr);
+            }
+            // 初始化sparseArr
+            sparseArr = new int[list.size()][3];
+            for (int i = 0; i < list.size(); i++) {
+                String[] splitStr = list.get(i).split("\t");
+                for (int j = 0; j < splitStr.length; j++) {
+                    sparseArr[i][j] = Integer.valueOf(splitStr[j]);
+                }
+            }
+
+            System.out.println("读取文件完成");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (buf != null) {
+                try {
+                    buf.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sparseArr;
+    }
+
+    /** 将稀疏数组保存到磁盘 */
+    public static void saveSparseArrToFile(int[][] sparseArr) {
+        OutputStream out = null;
+        try {
+            // 获取文件保存路径
+            String currentJavaFilePath = getCurrentJavaFilePath();
+            File file = new File( currentJavaFilePath + "map.txt");
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+
+            // 遍历稀疏数组将数据保存到文件中
+            out = new FileOutputStream(file, false);
+            for(int[] row : sparseArr) {
+                for(int data : row) {
+                    out.write(String.valueOf(data).getBytes());
+                    out.write('\t');
+                }
+                // 写完一行
+                out.write('\n');
+            }
+
+            System.out.println("写入文件完成");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.flush();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /** 获取当前java文件的路径 */
+    public static String getCurrentJavaFilePath() {
+        String filePath = "D:\\code_yl\\";
+        try {
+            File file = new File(SparseArray.class.getName());
+            String absolutePath = file.getAbsolutePath();
+            int i = absolutePath.lastIndexOf('\\');
+            filePath = absolutePath.substring(0, i + 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return filePath;
+    }
+
+
+}
